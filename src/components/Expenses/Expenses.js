fcc24bc3a7842e37919222d013eb8b943830de82
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-02-03 12:03:05 +0530

    Controlled Components - Expenses Filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem'
+import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+
 
 
 function Expenses({expenses}){
+  const [filteredYear,setFilteredYear]=useState('2023')
+  const changeFilterHandler=(year)=>{
+    setFilteredYear(year);
+  }
     return( 
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
         {expenses.map((expense) => (
               <ExpenseItem
                 key={expense.id}
